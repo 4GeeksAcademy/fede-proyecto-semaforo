9dfebd9a6ef0b941799421b331c6811d71e03fa8
Federico ferreyra <128550575+fedepaino@users.noreply.github.com>
2025-03-24 11:46:07 +0000

    semafor creado y con stilos de bootstap

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,41 @@
 import React from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+
+
+
+
 const Home = () => {
+
+	const [luzColor, setLuzColor] = useState(["red", "yellow", "green"]);
+	const [encenderLuz, setEncenderLuz] = useState(null);
+
+
+	function cambiarLuz(index) {
+		setEncenderLuz(index);
+	}
+
+
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			
+			<div className="bg-dark d-flex flex-column justify-content-center align-items-center m-3 py-3" style={{width: "100px"}}>
+				{luzColor.map((color, index) =>
+					<div
+						key={index}
+						style={{
+							background: color,
+							width: "50px", height: "50px",
+							margin: "10px",
+							borderRadius: "50%",
+							boxShadow: encenderLuz === index ? `0px 0px 20px 10px ${color}` : "none",
+						}} onClick={() => cambiarLuz(index)}></div>
+				)}
+				<div className="palo"></div>
+			</div>
 		</div>
+
 	);
 };
 
